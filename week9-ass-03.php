740509ba946f54fb401511d41333e7194defd71d
chimingg <68934510+chimingg@users.noreply.github.com>
2020-10-07 13:05:35 +0700

    Add files via upload

---
+++ week9-ass-03.php
@@ -0,0 +1,13 @@
+<?php
+  $filename = $_SERVER['argv'][1];
+  $handle = fopen($filename, "r");
+  $contents = [];
+  while(!feof($handle)) {
+    $contents[] = trim(fgets($handle));
+  }
+  $data = implode(" ", $contents);
+  $pattern = "/\(([A-Z]+)\)/";
+  preg_match_all($pattern,"$data", $matched);
+  for($i = 0; $i < count($matched[1]); $i++) {
+      echo "{$matched[1][$i]}\n";
+}
